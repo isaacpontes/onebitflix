879bc0fd544aecadaaf905e1b752bee966a9d965
Isaac <isaaclfp@outlook.com>
2022-02-04 18:51:43 -0300

    Changed course thumbnails bucket to public

--- src/adminjs/resources/course.ts
+++ src/adminjs/resources/course.ts
@@ -14,7 +14,7 @@ const courseResourceFeatures: FeatureType[] = [
   uploadFileFeature({
     provider: {
       local: {
-        bucket: path.join(__dirname, '../../../uploads')
+        bucket: path.join(__dirname, '../../../public')
       }
     },
     properties: {
