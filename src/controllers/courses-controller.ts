e60e41eb92101c853d0ffa2cc03bbc07ebeb4e7a
Isaac <isaaclfp@outlook.com>
2022-02-07 18:27:01 -0300

    Added search courses by name endpoint

--- src/controllers/courses-controller.ts
+++ src/controllers/courses-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { getPaginationParams } from '../helpers/get-pagination-params'
 import { courseService } from '../services/course-service'
 
 const coursesController = {
@@ -20,6 +21,21 @@ const coursesController = {
     try {
       const featuredCourses = await courseService.getRandomFeaturedCourses()
       return res.json(featuredCourses)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // GET /courses/search
+  search: async (req: Request, res: Response) => {
+    const { name } = req.body
+    const [page, perPage] = getPaginationParams(req.query)
+
+    try {
+      const courses = await courseService.findByName(name, page, perPage)
+      return res.json(courses)
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
--- src/services/course-service.ts
+++ src/services/course-service.ts
@@ -1,4 +1,5 @@
-import { Course } from "../models"
+import { Op } from 'sequelize'
+import { Course } from '../models'
 
 const courseService = {
   findByIdWithEpisodes: async (id: string) => {
@@ -25,6 +26,28 @@ const courseService = {
     const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random())
 
     return randomFeaturedCourses.slice(0, 4)
+  },
+
+  findByName: async (name: string, page: number, perPage: number) => {
+    const offset = (page - 1) * perPage
+
+    const { count, rows } = await Course.findAndCountAll({
+      attributes: ['id', 'name', 'synopsis', 'thumbnail_url'],
+      where: {
+        name: {
+          [Op.iLike]: `%${name}%`
+        }
+      },
+      limit: perPage,
+      offset
+    })
+
+    return {
+      courses: rows,
+      page,
+      perPage,
+      total: count
+    }
   }
 }
 
