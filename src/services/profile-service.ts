d9e4b4d5767ebd953c2e033327b2dbbc23490ac0
Isaac <isaaclfp@outlook.com>
2022-02-10 16:45:23 -0300

    Limited number of profiles.

--- src/services/profile-service.ts
+++ src/services/profile-service.ts
@@ -2,6 +2,12 @@ import { Profile } from "../models"
 
 const profileService = {
   create: async (name: string, avatar_url: string, user_id: number) => {
+    const profilesCount = await Profile.count({ where: { user_id } })
+
+    if (profilesCount >= 4) {
+      throw new Error('Número máximo de perfis atingido')
+    }
+
     const profile = await Profile.create({
       name,
       avatar_url,
